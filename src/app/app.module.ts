be10e69a69c0998b4e5b3ebe17934a1317cd4d98
Neelanshi <neelanshibhatnagar@ymail.com>
2019-08-07 16:55:27 +0530

    two way binding in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Component2Component } from './component2/component2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component2/component2.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.css']
+})
+export class Component2Component implements OnInit {
+
+  public name="";
+  public displayname=true;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  submit(){
+    this.displayname = false;
+  }
+}
